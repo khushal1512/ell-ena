3817a9fcec3d9f8f6cc6ce927695b8b337a5169c
khushal1512 <khushal080424@gmail.com>
2025-04-18 19:06:54 +0530

    Implement Meetings Page, Meeting Component & Database Connection Utility

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,1 +1,25 @@
-import
+import mongoose from 'mongoose'; 
+
+let isConnected: boolean = false; 
+
+export const connectToDatabase = async () => {
+    mongoose.set('strictQuery', true); 
+    if(!process.env.MONGO_DB_URL) return console.log('Missing DB URL');
+
+    if(isConnected) {
+        return console.log('MongoDB is connected');
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URL, {
+            dbName: 'ellena'
+        })
+
+        isConnected = true; 
+
+    } catch(error) {
+        console.log(error)
+        throw error;
+
+    }
+}
